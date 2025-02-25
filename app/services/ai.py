69a9cc08e7ba5786e66159b5d929f04c68ede5d0
CmilAmaya <macamayar@gmail.com>
2025-02-24 22:57:02 -0500

    feat: new prompt

--- app/services/ai.py
+++ app/services/ai.py
@@ -2,7 +2,7 @@ import os
 import google.generativeai as genai
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.5-pro")
 
 def ask_gemini(prompt: str):
     response = model.generate_content(prompt)
